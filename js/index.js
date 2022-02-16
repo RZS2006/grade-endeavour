320c2d081cb4185fa05009d9a7326dbae281ec52
Raiyan Zarif <razash139@gmail.com>
2022-02-16 18:52:48 +0200

    fix styles

--- js/index.js
+++ js/index.js
@@ -1,6 +1,35 @@
 const addCourseForm = document.querySelector('#add-course-form');
+const courseContainer = document.querySelector('#course-container');
+
+const addCourse = (name, goal) => {
+	const node = document.createElement('div');
+	const textnode = document.createTextNode(`${name}: ${goal}`);
+	node.appendChild(textnode);
+	courseContainer.appendChild(node);
+};
+
+const deleteCourse = (id) => {
+	return;
+};
+
+const addCourseItem = (name, type, date, grade, courseId) => {
+	return;
+};
+
+const deleteCourseItem = (itemId, courseId) => {
+	return;
+};
 
 addCourseForm.addEventListener('submit', (e) => {
 	e.preventDefault();
-	console.log('hah');
+
+	const courseName = addCourseForm.querySelector('#course-name').value;
+	const courseGoal = parseFloat(
+		addCourseForm.querySelector('#course-goal').value
+	);
+
+	if (courseName.length > 0 && courseGoal >= 4 && courseGoal <= 10) {
+		addCourse(courseName, courseGoal);
+		console.log(`${courseName}: ${courseGoal}`);
+	}
 });
